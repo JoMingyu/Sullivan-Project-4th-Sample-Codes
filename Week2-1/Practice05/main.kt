8b35ab7385a1ec00abd0a379d027f9aa75004bb4
JoMingyu <city7310@naver.com>
2018-05-13 07:16:24 +0900

    Week 2-1 - Practice 02..06

---
+++ Week2-1/Practice05/main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    for(i in 1..100) {
+        if(i % 2 == 1) {
+            println(i)
+        }
+    }
+}
---
+++ Week2-1/Practice06/main.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    for(i in 1..9) {
+        println("9 * ${i} = ${9 * i}")
+    }
+}
