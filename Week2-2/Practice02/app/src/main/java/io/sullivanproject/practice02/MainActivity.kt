5bcfa319606dfa060e46642fa0559fc1629df824
DaeunJeong <alal8272@gmail.com>
2018-05-13 07:43:11 +0900

    구조 바꿨따^^

---
+++ Week2-2/Practice02/app/src/main/java/io/sullivanproject/practice02/MainActivity.kt
@@ -0,0 +1,21 @@
+package io.sullivanproject.practice02
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var number = 0
+
+        button.setOnClickListener {
+            number = number + 1
+            textView.setText(number.toString())
+        }
+
+    }
+}
