f3eb132b1c1ffa172aa9043f79b0b9a3b0f68404
JoMingyu <city7310@naver.com>
2018-05-13 07:03:52 +0900

    Week 1-2 - Practice05

---
+++ Week1-2/Practice05/main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println(1 + 3)
+
+    var a = 1
+    var b = 2
+
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+    println(a % b)
+}
